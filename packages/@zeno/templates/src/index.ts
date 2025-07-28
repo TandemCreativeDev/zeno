4fbb2d2b7f5e275bd6630efa896646caad38878f
maxitect <maxime.downe@gmail.com>
2025-07-28 23:50:25 +0100

    chore(templates): src index.ts export file

---
+++ packages/@zeno/templates/src/index.ts
@@ -0,0 +1,8 @@
+// Re-export commonly used types from dependencies for convenience
+export type {
+  FieldErrors,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
+export * from "./components";
