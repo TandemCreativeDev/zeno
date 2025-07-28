d0b47d75455343d9fd35331560d3ad46ffff5839
maxitect <maxime.downe@gmail.com>
2025-07-28 14:06:56 +0100

    feat(core): export logging utilities from utils index
    Add proper exports for Logger, CliLogger classes and related types.
    Maintains alphabetical import order following project conventions.

--- packages/@zeno/core/src/utils/index.ts
+++ packages/@zeno/core/src/utils/index.ts
@@ -2,6 +2,8 @@
  * @fileoverview Utility functions for Zeno core package
  */
 
+export type { CliLoggerOptions } from "./cliLogger";
+export { CliLogger, createCliLogger } from "./cliLogger";
 export {
   DEFAULT_CONFIG,
   defineConfig,
@@ -24,6 +26,8 @@ export {
   safeWriteFile,
   validatePath,
 } from "./fileSystem";
+export type { LogEntry, LoggerOptions, LogLevel } from "./logger";
+export { createLogger, Logger } from "./logger";
 export { createSchemaDiffer, SchemaDiffer } from "./schemaDiff";
 export { createSchemaLoader, SchemaLoader } from "./schemaLoader";
 export { createWatcher, Watcher } from "./watcher";
